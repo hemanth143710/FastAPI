8494379fb203e9ee691f24b6fa3c2512d9f5ba5d
hemanth1437 <hemanthrockzzing@gmail.com>
2022-05-17 19:10:58 +0530

    chatapp

--- BAICS/blog/main.py
+++ BAICS/blog/main.py
@@ -1,5 +1,4 @@
-
-from urllib import response
+from typing import List
 from fastapi import FastAPI ,Depends, status ,Response, HTTPException
 from pydantic import BaseModel
 from . import schemas, models
@@ -27,7 +26,7 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
     
-@app.get('/blog')
+@app.get('/blog',response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
@@ -43,12 +42,18 @@ def show(id, response:Response, db: Session = Depends(get_db)):
 
 @app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db : Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with id {id} not found")
+    blog .delete(synchronize_session=False)
     db.commit()
     return 'done'
-
+ 
 @app.put('/blog/{id}')
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).update(request)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with id {id} not found")
+    blog.update(request)
     db.commit()
     return "updated"
--- BAICS/blog/schemas.py
+++ BAICS/blog/schemas.py
@@ -5,3 +5,11 @@ class Blog(BaseModel):
     title: str
     body: str
     published: Optional[bool]
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str 
+
+    class Config():
+        orm_mode = True
+    
