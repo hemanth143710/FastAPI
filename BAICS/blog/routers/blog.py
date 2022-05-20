ee821471b99ba870a897027b8ca4186ecc5f903c
hemanth1437 <hemanthrockzzing@gmail.com>
2022-05-20 19:04:56 +0530

    fourth

--- BAICS/blog/routers/blog.py
+++ BAICS/blog/routers/blog.py
@@ -32,12 +32,13 @@ def show(id,  db: Session = Depends(get_db)):
 
 @router.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT,tags=['blog'])
 def destroy(id, db : Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with id {id} not found")
-    blog .delete(synchronize_session=False)
-    db.commit()
-    return 'done'
+    # blog = db.query(models.Blog).filter(models.Blog.id == id)
+    # if not blog.first():
+    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with id {id} not found")
+    # blog .delete(synchronize_session=False)
+    # db.commit()
+    # return 'done'
+    return blog.destroy(id,db)
  
 @router.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED,tags=['blog'])
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
